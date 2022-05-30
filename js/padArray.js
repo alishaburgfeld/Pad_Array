31525caa859bf5ddb3253964ca989a982c206e15
alishaburgfeld <alisha.burgfeld@gmail.com>
2022-05-30 10:50:58 -0400

    added JS and PY

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,16 @@
 // REMEMBER TO PSEUDOCODE
-const pad = (array, minSize, value=null) => {
+// const pad = (array, minSize, value=null) => {
 
+// }
+
+// find length of Array, if length is less than minsize, push the filler word (while loop)
+
+function pad(array, minSize, value=null) {
+    let answer =[...array]
+    while (answer.length < minSize) {
+        answer.push(value)
+    }
+    return answer
 }
+console.log(pad([1,2,3], 5))
+console.log(pad([1,2,3], 5, 'apple'))
--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,14 @@
 #REMEMBER TO PSEUDOCODE
+
+# find length of Array, if length is less than minsize, push the filler word (while loop)
 def pad(array, min_size, value = None):
+    answer=array.copy()
+    while len(answer) < min_size:
+        answer.append(value)
+    return answer
+
+print(pad([1,2,3], 5))
+print(pad([1,2,3], 5, 'apple'))
+print(pad([1,2,3], 3, 'apple'))
+
+
